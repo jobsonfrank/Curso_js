61756238f32dc89e65e678fb079119243a7c5c1e
Jobson Frank Floriano <94191561+jobsonfrank@users.noreply.github.com>
2022-01-19 00:03:36 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 12.5 
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60.5) {
+    console.log(`Você passou o limite e foi multado!`)
+}
+console.log(`Dirija usando sempre cintode segurança !`)
