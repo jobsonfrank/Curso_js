c1600bae7c6d6999eed63e2628d6f1cddbd811fa
Jobson Frank Floriano <94191561+jobsonfrank@users.noreply.github.com>
2022-01-31 21:54:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-function carregar(){
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
+function verificar(){
 
-msg.innerHTML = `Agora são ${hora} horas `
-if (hora >= 0 && hora < 12){
-    img.src = 'fotomanha.jpg'
-    document.body.style.background = '#e68686'
-}else if(hora >= 12 && hora <= 18){
-    img.src = 'fototarde.jpg'
-    document.body.style.background = '#e91919 '
-}else{
-    img.src = 'fotonoite.jpg'
-    document.body.style.background = '#363636'
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano') 
+   var res = document.querySelector('div#res')
+   if(fano.Value == 0 || Number(fano.Value) > ano){
+      window.alert('[ERRO] Verifique os dados e tente novamente')
+   }else{
+       var fsex = document.getElementsByName('radsex')
+       var idade  = ano - Number(fano.Value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if (fsex[0].checked){
+           genero = 'Homem'
+           if(idade >= 0 && idade < 10){
+               //criança
+               img.setAttribute('src', 'criança_masc.jpg')
+
+           }else if(idade > 10 && idade < 21){
+               //jovem
+               img.setAttribute('src', 'jovem_masc.jpg')
+
+           }else if(idade > 21 && idade < 50){
+               //Adulto
+               img.setAttribute('src', 'adulto_masc.jpeg')
+
+               
+           }else{
+               //Idoso
+               img.setAttribute('src', 'idoso_masc.jpg')
+           }
+       }else if(fsex[1].checked){
+           genero = 'Mulher'
+           if(idade >= 0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'criança_fem.jpg')
+        }else if(idade < 21){
+            //jovem
+            img.setAttribute('src', 'jovem_fem.jpg')
+        }else if(idade < 50){
+            //Adulto
+            img.setAttribute('src', 'adulto_fem.jpeg')
+        }else{
+            //Idoso
+            img.setAttribute('src', 'idoso_fem.jpg')
+        }
+       }
+       res.getElementsByClassName.textAling = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+       res.appendChild(img)
+       
+   }
 }
-}
-//2cd9f
